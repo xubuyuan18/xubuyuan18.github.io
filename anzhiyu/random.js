8c4cb63885f523a9da5a7615696eda2fe2a73c88
xubuyuan18 <xubuyuan18@users.noreply.github.com>
2026-05-30 17:18:49 +0000

    deploy: 24ef821dc0830b569273b0f2756ee05d2ee14c06

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/03/21/GRUB2换主题踩坑记/","2024/06/20/GRUB手抖删分区/","2026/05/30/HEXO折腾日记/","2025/05/30/Linux娘守护日记/","2026/04/30/Linx CVE-2026-31431/","2024/05/30/hexo/","2025/06/01/华为ICT比赛记录/"];function toRandomPost(){
+var posts=["2024/06/20/GRUB手抖删分区/","2026/05/30/HEXO折腾日记/","2024/03/21/GRUB2换主题踩坑记/","2026/04/30/Linx CVE-2026-31431/","2025/06/01/华为ICT比赛记录/","2025/05/30/Linux娘守护日记/","2024/05/30/hexo/","2026/04/10/基于K8s节点RAG本地知识库问答系统/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
